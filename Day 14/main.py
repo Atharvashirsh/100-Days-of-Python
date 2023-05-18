b6e3c7be6545f04667187d0f00c882252bb5e380
Atharvashirsh <astw21209@gmail.com>
2023-05-18 19:58:52 +0530

    Update code

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,3 +1,37 @@
 import random
-import game_data
+from game_data import data
 import art
+import os
+
+# Clearing the screen
+os.system("cls")
+
+# Print logo
+print(art.logo)
+
+
+def generate_person():
+    index = random.randint(0, len(data))
+
+    person_name = data[index]["name"]
+    followers = data[index]["follower_count"]
+    details = data[index]["description"]
+    country_name = data[index]["country"]
+
+    print(f"Compare A : {a_person_name}, a {a_details}, from {a_country_name}")
+
+
+def compare():
+    {}
+
+
+print(art.vs)
+
+b_person_name = data[1]["name"]
+b_followers = data[1]["follower_count"]
+b_details = data[1]["description"]
+b_country_name = data[1]["country"]
+
+print(f"Against B : {b_person_name}, a {b_details}, from {b_country_name}")
+
+print("Who has more followers? Type 'A' or 'B': ")
