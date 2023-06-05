0d6c6adf19f5a4f27c6cae544779055e91bc5680
Atharvashirsh <astw21209@gmail.com>
2023-06-05 23:36:11 +0530

    Updated files

--- Day 23/main.py
+++ Day 23/main.py
@@ -8,10 +8,6 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-
-# Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge of the screen. No cars should be generated in the top and bottom 50px of the screen
-#  (think of it as a safe zone for our little turtle). Hint: generate a new car only every 6th time the game loop runs.
-
 # Detect when the turtle player collides with a car and stop the game if this happens
 
 # Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When this happens, return the turtle to the starting position and increase the speed of the cars
@@ -21,6 +17,7 @@ screen.tracer(0)
 #  When the turtle hits a car, GAME OVER should be displayed in the centre
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 
@@ -29,3 +26,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_all_cars()
