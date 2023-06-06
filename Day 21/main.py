d79f5795d8950d613b7061aac62bec55cccf7279
Atharvashirsh <astw21209@gmail.com>
2023-06-06 20:38:55 +0530

    Added highscore feature

--- Day 21/main.py
+++ Day 21/main.py
@@ -49,13 +49,13 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        game_is_on = False
-        score.game_over()
+        score.reset_score()
+        snake.reset_snake()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            snake.reset_snake()
+            score.reset_score()
 
 screen.exitonclick()
