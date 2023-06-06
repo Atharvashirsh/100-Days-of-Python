490605b31a450d2e74c10e747ab39f7af7618534
Atharvashirsh <astw21209@gmail.com>
2023-06-06 21:04:54 +0530

    Added highscore file

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -8,7 +8,9 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as file:
+            self.highscore = int(file.read())
+
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -30,6 +32,8 @@ class Score(Turtle):
     def reset_score(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
 
         self.score = 0
         self.update_scoreboard()
