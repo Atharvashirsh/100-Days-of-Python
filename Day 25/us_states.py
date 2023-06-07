9327ab22430eb82997cd4a0bacc0f59b74ffc040
Atharvashirsh <astw21209@gmail.com>
2023-06-08 00:08:57 +0530

    Modified Day 25 files

--- Day 25/us_states.py
+++ Day 25/us_states.py
@@ -6,26 +6,37 @@ screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-
-
 writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
 
-guess = screen.textinput(
-    title="Guess the state", prompt="What's another state's name?"
-).title()
-# print(answer_state)
+
+guessed_states = []
+
 
 states = pandas.read_csv("50_states.csv")
-# print(states["state"] == guess)
 
-if guess in states["state"].to_list():
-    data = list(states[states["state"] == guess])
-    print(data)
-    writer.penup()
-    writer.hideturtle()
-    print(data["state"], int(data["x"]), int(data["y"]))
-    writer.goto(int(data.x), int(data.y))
-    writer.write(data["state"], move=False, font=("Arial", 8, "normal"))
+while len(guessed_states) < 50:
+    guess = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What's another state's name?",
+    ).title()
 
+    if guess == "Exit":
+        break
 
-screen.exitonclick()
+    if guess in states["state"].to_list():
+        guessed_states.append(guess)
+        data = states[states["state"] == guess]
+        writer.goto(int(data.x), int(data.y))
+        writer.write(data["state"].item())
+
+# states_to_learn.csv
+
+states_to_learn = []
+for state in states["state"].to_list():
+    if state not in guessed_states:
+        states_to_learn.append(state)
+
+new_data = pandas.DataFrame(states_to_learn)
+new_data.to_csv("states_to_learn.csv")
